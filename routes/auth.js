dfe772a51c2506b1ca7e4cd3d2559347da00a22f
Ryan Hufford <ryan@rmhufford.com>
2023-10-18 21:35:24 -0400

    init'd and started auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
+const db = require('../db');
 
 const router = express.Router();
 
 router.get('/login', (req, res, next) => {
   res.render('login');
 })
+
+router.get('/login/federated/google', passport.authenticate('google'));
 
 module.exports = router;
